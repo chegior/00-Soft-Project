e1882495d4196485d243174c433c6d22a420de5b
chegior <chegiorca@hotmail.com>
2017-02-24 23:57:31 -0700

    Initial Program
    This is a TEMPLATE that is working

---
+++ App.js
@@ -0,0 +1,20 @@
+var newArr;
+var filtered = [12, 5, 8, 130, 44];
+
+// THIS SECTION IS THE CODE 
+
+function checkId(value) {
+    if ( value > 10)
+     return value;
+}
+function bouncer(arr) {
+    arr = filtered.filter(checkId);
+    return arr;
+}
+
+newArr = bouncer(filtered);
+
+//THIS SECTION PRINT THE RESULTS
+
+for (var i = 0; i < newArr.length; i++ )
+console.log(newArr[i]);
